e985bd521eef1f4c665a16d38b45eeabec0f9e9b
colinelder <user.name>
2024-02-14 14:07:06 -0500

    attempted parsing function

--- src/WorkingCode.py
+++ src/WorkingCode.py
@@ -7,3 +7,12 @@ import os
 nlp = spacy.load("en_core_web_sm")
 
 # Parsing function
+
+def text_parsing(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        text = file.read()
+        doc = nlp(text)
+        return doc
+
+# Tensorizer - this gives words parts of speech
+
