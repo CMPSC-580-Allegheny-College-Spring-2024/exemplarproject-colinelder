c5edf791fa043ec693cd0372bd876ae0bb7ca658
colinelder <user.name>
2024-02-19 14:11:33 -0500

    added code to iterate through files

--- src/WorkingCode.py
+++ src/WorkingCode.py
@@ -2,11 +2,32 @@
 
 import spacy
 import os
+import re
+import streamlit as st
 
-# Add tokenizer so that spacy 
+# Add tokenizer so that spacy can assign vectors to words
 nlp = spacy.load("en_core_web_sm")
 
-# 
+# define file path to text directories
+data_path = os.path.join(os.path.dirname(__file__), '..', '..','exemplar_data')
+
+# Creates empty list for text file paths
+text_path = []
+
+# Function to remove any non letter characters in the text
+def remove_non_letters(text):
+    # This uses regular expression to remove non-letter characters from text files
+    return re.sub(r'[^a-zA-Z]', '', text)
+
+# Program needs to iterate through files
+
+for filename in os.listdir(data_path):
+        file_path = os.path.join(data_path, filename)
+        with open(file_path, 'r') as file:
+            # Process content of the file here
+            content = file.read()
+            # Example: print the filename
+            print(f"File: {filename}")
 
 # Parsing function
 
